e8eded291a7745318cbda8b71747419e01ba1b2a
Javier <50588184+Javier767@users.noreply.github.com>
2024-10-28 20:12:11 -0600

    Add files via upload

---
+++ Scripts/Scripts DimTiempo/Creacion DimTiempo.sql
@@ -0,0 +1,16 @@
+
+create table DimTiempo (
+dateKey int not null,
+date smalldatetime,
+dayOfWeek tinyint,
+dayOfMonth tinyint,
+dayOfYear smallint,
+nameOfDay varchar(12),
+weekNumberByYear tinyint,
+weekNumber smallint,
+weekFirstDay datetime,
+weekFirstDayKey int,
+monthNumber tinyint,
+monthName varchar(12),
+year smallint,
+primary key (dateKey))
